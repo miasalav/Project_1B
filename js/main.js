31f94a53d19485ffb2bc73fece49be05f0fc0335
miasalav <miaria00@gmail.com>
2016-10-20 10:13:53 -0400

    Checking the optimization by minifying css.

--- js/main.js
+++ js/main.js
@@ -34,13 +34,44 @@ $(document).ready(function(){
   ]
 });
  
- $('#cart-items').hide();
+ $('#cart-num').hide();
 
-$('.product-div').on("click", function(e) {  
-     var sale = $(this).data("sale");
+function buildProduct(result){
+
+
+   for(i in result.products){
+
+    var clone = $('.product-div').eq(0).clone();
+
+    var productName = (result.products[i].name);
+    var productImg = (result.products[i].image);
+    var regPrice = (result.products[i].regularPrice);
+    var salePrice = (result.products[i].salePrice);
+
+    $(clone).children('a').children('img').attr("src", productImg); 
+       // $('article').children('img').attr("src", final_image);
+    $(clone).children('a').children('h5').html(productName);
+    $(clone).children('a').children('p').html(salePrice);
+        
+    $('.product-container').append(clone);
+
+    //console.log(productName, productImg, regPrice, salePrice);
+
+    
+
+     }
+
+     $('.product-div').on("click", function(e) {  
+    e.preventDefault();
+
+    console.log('click works');
+
+    var sale = $(this).data("sale");
      var price = $(this).data("price");
      var discount = $(this).data("discount");
      var discountPrice = (price - discount);
+
+     console.log('click works');
           
      if(sale == true){
          alert("this product is on sale original price: " + price + " discount price: " + discountPrice);
@@ -49,20 +80,32 @@ $('.product-div').on("click", function(e) {
          alert("this product is not on sale");
      };
   
-     $('#cart-items').show();
-     var currentTotal = parseInt($('#cart-items').html());
+     $('#cart-num').show();
+     var currentTotal = parseInt($('#cart-num').html());
      var newTotal = currentTotal + 1;
-     $('#cart-items').text(newTotal);    
- 
+     $('#cart-num').text(newTotal); 
+
+     })
+
+
+     $('.menu-img').on("click", function(e) { 
+     })
+
+
+}
+var url = "http://api.remix.bestbuy.com/v1/products((categoryPath.id=abcat0501000))?apiKey=8ccddf4rtjz5k5btqam84qak&format=json&show=image,name,regularPrice,salePrice";
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(result) {
+        buildProduct(result);
+             
+    }).fail(function(err) {
+        throw err;
     });
-
-
-    console.log("email field's value is" + email.value);
-        console.log('email fields value is = ' + document.getElementById("email").type);
-       // console.log('');
+    
 
      
-
  });
 
  
